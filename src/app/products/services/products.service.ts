2d29e6df4b1355cea0013855e186a0fc30ba65a4
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-02-03 18:33:20 +0100

    247. Variablews de entorno y parametrizacion

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -2,16 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ProductsResponse } from '@products/interfaces/product.interface';
 import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+
+interface Options {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private http = inject(HttpClient);
 
-  getProducts(): Observable<ProductsResponse> {
-    return this.http.get<ProductsResponse>('http://localhost:3000/api/products').pipe(
-      tap((resp) => {
-        console.log(resp);
-      }),
-    );
+  getProducts(options: Options): Observable<ProductsResponse> {
+    const { limit = 10, offset = 0, gender = '' } = options;
+
+    return this.http
+      .get<ProductsResponse>(`${baseUrl}/products`, {
+        params: {
+          limit: limit,
+          offset: offset,
+          gender: gender,
+        },
+      })
+      .pipe(
+        tap((resp) => {
+          console.log(resp);
+        }),
+      );
   }
 }
