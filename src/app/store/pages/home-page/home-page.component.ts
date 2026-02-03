684849071818c50466202fe96745dc955f70bb9f
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-02-03 17:47:22 +0100

    246. Traer listado de productos

--- src/app/store/pages/home-page/home-page.component.ts
+++ src/app/store/pages/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/products.service';
 // import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
 
 @Component({
@@ -7,4 +9,13 @@ import { ProductCardComponent } from '@products/components/product-card/product-
   imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => {
+      return this.productsService.getProducts();
+    },
+  });
+}
