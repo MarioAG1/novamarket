c76133c1002a0ba360aee2a04686ab0fd7c964d7
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-02-04 13:13:31 +0100

    250. Tarea Pipe - Mostrar Imagenes del producto (Repasarla)

---
+++ src/app/products/pipes/product-image.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+
+@Pipe({
+  name: 'productImage',
+})
+export class ProductImagePipe implements PipeTransform {
+  transform(value: string | string[]): string {
+    if (!value) {
+      return './assets/images/no-image.jpg';
+    }
+
+    if (typeof value === 'string') {
+      return `${baseUrl}/files/product/${value}`;
+    }
+
+    return `${baseUrl}/files/product/${value[0]}`;
+  }
+}
