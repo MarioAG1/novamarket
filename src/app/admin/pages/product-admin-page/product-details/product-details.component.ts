15534bcdc154850b67abc2fcb9683c7438793a5e
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-02-19 18:51:08 +0100

    295. Mostrar errores del formulario

--- src/app/admin/pages/product-admin-page/product-details/product-details.component.ts
+++ src/app/admin/pages/product-admin-page/product-details/product-details.component.ts
@@ -3,10 +3,11 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductCarouselComponent } from '@products/components/product-carousel/product-carousel.component';
 import { Product } from '@products/interfaces/product.interface';
 import { FormUtils } from '@utils/form-utils';
+import { FormErrorLabelComponent } from '@shared/components/form-error-label/form-error-label.component';
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarouselComponent, ReactiveFormsModule],
+  imports: [ProductCarouselComponent, ReactiveFormsModule, FormErrorLabelComponent],
   templateUrl: './product-details.component.html',
 })
 export class ProductDetailsComponent implements OnInit {
@@ -46,6 +47,7 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   onSumbit() {
-    console.log(this.productForm.value);
+    const isValid = this.productForm.valid;
+    console.log(this.productForm.value, { isValid });
   }
 }
